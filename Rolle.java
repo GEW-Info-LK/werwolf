c15937ec28a7af0d53f7fd4d44640d414126ea4d
leo <leonardo.blume@gymnasium-essen-werden.de>
2022-08-30 18:51:05 +0200

    Rolle::istSpieler als automatische Negation

--- Rolle.java
+++ Rolle.java
@@ -2,6 +2,9 @@ import java.util.*;
 public abstract class Rolle
 {
     public abstract boolean istWerwolf();
+    public boolean istSpieler() {
+        return !istWerwolf();
+    }
     @Override
     public abstract String toString();
 }
